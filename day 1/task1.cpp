3c5b4a1c74c500e7a405767c41514b6a2c571d0d
George Long <george@longhouse.dev>
2022-12-02 20:08:00 +0000

    Day 1 tasks completed

---
+++ day 1/task1.cpp
@@ -0,0 +1,46 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+
+int main(){
+
+    // List containing how many calories each elf is carrying
+    std::vector<int> elves;
+
+    int current_cals = 0;
+    int total_cals = 0;
+    std::string line;
+
+    std::fstream fs ("input.txt", std::fstream::in);
+
+    std::cout << "Test 1";
+    if (fs.is_open()) {
+        while(std::getline(fs, line)) {
+
+            //There is a space, new elf
+            if (line == "") {
+
+                elves.push_back(current_cals);
+                current_cals = 0;
+            }
+            // Count calories
+            else {
+                current_cals += stoi(line);
+            }
+        }
+
+        std::sort(elves.begin(), elves.end());
+
+        for (std::vector<int>::iterator it=elves.end(); it > elves.end()-4; it--) {
+            std::cout << *it << "\n";
+            total_cals += *it;
+        }
+
+        std::cout << "The three elves are carrying " << total_cals << " calories.";
+        fs.close();
+        return 0;
+    }
+}
